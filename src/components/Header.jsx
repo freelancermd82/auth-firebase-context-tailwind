2a3900491a6f1e89069369b060503e83d602ec2c
Your Name <freelancermd82@gmail.com>
2024-08-01 06:51:21 +0600

    login and registation signOut

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../providers/AuthProviders';
 
 const Header = () => {
+
+    const { user, logOut } = useContext(AuthContext);
+
+
+    const handleLogOut = () => {
+        logOut()
+            .then(() => { })
+            .catch(error => console.log(error));
+    }
+
     return (
         <div>
             <div className="navbar bg-primary text-primary-content">
@@ -9,6 +20,13 @@ const Header = () => {
                 <Link className="btn btn-ghost text-xl" to="/">Home</Link>
                 <Link className="btn btn-ghost text-xl" to="/login">Login</Link>
                 <Link className="btn btn-ghost text-xl" to="/register">Register</Link>
+
+                {
+                    user ? <>
+                        <span>{user.email}</span>
+                        <button onClick={handleLogOut} className="btn btn-xs">Sign out</button>
+                    </> : <><Link to="/login">Login</Link></>
+                }
             </div>
         </div>
     );
